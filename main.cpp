2779880a0df358e161df8e367955f33cb3c062f1
sohitfirs <dokukin.i@mail.ru>
2023-03-09 11:13:30 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+/* https://github.com/sohitfirs/4.3.git */
+
+#include <stdio.h>
+#include <cstdlib>
+
+main()
+{
+	int mas[100];
+	int n;
+	
+	printf("kol el ");
+	scanf("%i", &n);
+	
+	for (int i=0; i<n; ++i)
+	{
+		scanf("%i", &mas[i]);
+	}
+	
+	for (int i=0; i<n; ++i)
+	{
+		printf("%4i", mas[i]);
+	}
+	printf("\n");
+	
+	bool fl=true;
+	
+	int ch=0;
+	while(fl)
+	{
+		fl=false;
+		for (int i=0; i<n-1; i++)
+		{
+			if(mas[i]>mas[i+1])
+			{
+				int z = mas[i];
+				mas[i]=mas[i+1];
+				mas[i+1]=z;
+				fl=true;
+			}
+		}
+		ch++;
+	}
+	
+	for (int i=0; i<n; ++i)
+	{
+		printf("%4i", mas[i]);
+	}
+	
+	printf("\n%i\n", ch);
+	system ("pause");
+}
